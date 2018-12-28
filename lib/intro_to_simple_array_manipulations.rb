7ed9b0e587af9eea6ead478094354329dcad8b6e
Ryan Gill <rdgill95@gmail.com>
2018-12-28 01:52:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,4 +23,13 @@ def shift_with_args(array)
 end
 
 def using_concat(array,array2)
-  
+  array.concat(array2)
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.using
+end
